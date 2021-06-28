1c396da791cfd6d1a1a62caf360d812035bf1e1d
Joelson Ferreira <joelsonferreira@id.uff.br>
2021-06-28 12:46:34 -0300

    adding responsiveness rules

--- scripts/index.js
+++ scripts/index.js
@@ -1,24 +1,36 @@
+function toggleMenu() {
+    const menu = document.querySelector(".nav-item:nth-child(3)");
+    const overlay = document.querySelector("#overlay");
+    const hamburguer = document.querySelector("#hamburguer");
+    
+    menu.classList.toggle("start");
+    overlay.classList.toggle("start");
+    hamburguer.classList.toggle("start");
+}
+
 // function to set a given theme/color-scheme
 function setTheme(themeName) {
     localStorage.setItem('theme', themeName);
     document.documentElement.className = themeName;
 }
 
-// function to toggle between light and dark theme
-function toggleTheme() {
-    if (localStorage.getItem('theme') === 'theme-light') {
-        setTheme('theme-dark');
-    } else {
-        setTheme('theme-light');
-    }
-}
+// // function to toggle between light and dark theme
+// function toggleTheme() {
+//     if (localStorage.getItem('theme') === 'theme-light') {
+//         setTheme('theme-dark');
+//     } else {
+//         setTheme('theme-light');
+//     }
+// }
 
-(function () {
-    if (localStorage.getItem('theme') === 'theme-dark') {
-        setTheme('theme-dark');
-        document.getElementById('slider').checked = false;
-    } else {
-        setTheme('theme-light');
-      document.getElementById('slider').checked = true;
-    }
-})();
+// (function () {
+//     if (localStorage.getItem('theme') === 'theme-dark') {
+//         setTheme('theme-dark');
+//         document.getElementById('slider').checked = false;
+//     } else {
+//         setTheme('theme-light');
+//       document.getElementById('slider').checked = true;
+//     }
+// })();
+
+setTheme("theme-light");
